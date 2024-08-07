2ef38752dc4fa2e7d0f91c27873333f0d131544a
FaydSpeare <fspea123@gmail.com>
2024-08-07 21:01:21 +0200

    youtube search endpoints

--- src/client.js
+++ src/client.js
@@ -287,15 +287,16 @@ class TikTokClient {
 
     /**
      * @typedef {{
-     *    keyword: string;
-     *    cursor: number;
-     *    sorting: "0" | "1" | "2" | "3" | "4";
-     *    filterBy: "0" | "1" | "2";
+     *     keyword: string;
+     *     cursor: number;
+     *     sorting: "0" | "1" | "2" | "3" | "4";
+     *     filterBy: "0" | "1" | "2";
      * }} TikTokMusicSearchParams
      */
 
     /**
      * Fetch information about music based on a string.
+     *
      * @param {TikTokMusicSearchParams} params
      * @returns {Promise<EDResponse>}
      */
@@ -309,18 +310,22 @@ class TikTokClient {
     }
 
     /**
-     * Fetch the videos which have a particular piece of music in the background.
-     * The `music_id` can be obtained from other endpoints.
+     * Fetch the videos which have a particular piece of music in the
+     * background. The `music_id` can be obtained from other endpoints.
      *
      * @param {{ musicId: string; cursor: number }} params
      * @returns {Promise<EDResponse>}
      */
     musicPosts({ musicId, cursor }) {
-        return this.#requester.get("/tt/music/posts", { music_id: musicId, cursor });
+        return this.#requester.get("/tt/music/posts", {
+            music_id: musicId,
+            cursor,
+        });
     }
 
     /**
      * Fetch detailed information for a music ID.
+     *
      * @param {string} musicId
      * @returns {Promise<EDResponse>}
      */
@@ -329,37 +334,54 @@ class TikTokClient {
     }
 
     /**
-     * Fetch followers for a given user. Each request returns a chunk of 100 followers.
+     * Fetch followers for a given user. Each request returns a chunk of 100
+     * followers.
+     *
      * @param {{ userId: string; secUid: string; cursor: number }} params
      * @returns {Promise<EDResponse>}
      */
     userFollowers({ userId, secUid, cursor }) {
-        return this.#requester.get("/tt/user/followers", { id: userId, secUid, cursor });
+        return this.#requester.get("/tt/user/followers", {
+            id: userId,
+            secUid,
+            cursor,
+        });
     }
 
     /**
-     * Fetch followings for a given user. Each request returns a chunk of 100 followings.
-     * @param {{ userId: string; secUid: string; cursor: number, pageToken: string }} params
+     * Fetch followings for a given user. Each request returns a chunk of 100
+     * followings.
+     *
+     * @param {{
+     *     userId: string;
+     *     secUid: string;
+     *     cursor: number;
+     *     pageToken: string;
+     * }} params
      * @returns {Promise<EDResponse>}
      */
     userFollowings({ userId, secUid, cursor, pageToken }) {
-        const params = { id: userId, secUid, cursor, ...toSnakeCaseObject({ pageToken }) };
+        const params = {
+            id: userId,
+            secUid,
+            cursor,
+            ...toSnakeCaseObject({ pageToken }),
+        };
         return this.#requester.get("/tt/user/followings", params);
     }
 
     /**
      * Fetch the list of user's liked posts (only if publicly available).
-     * @param {{ secUid: string; cursor: number }} params 
+     *
+     * @param {{ secUid: string; cursor: number }} params
      * @returns {Promise<EDResponse>}
      */
     userLikedPosts({ secUid, cursor }) {
         return this.#requester.get("/tt/user/liked-posts", { secUid, cursor });
     }
-
 }
 
 class InstagramClient {
-
     /**
      * @type {Requester}
      * @readonly
@@ -373,39 +395,58 @@ class InstagramClient {
 
     /**
      * @typedef {{
-     *    userId: number,
-     *    depth: number,
-     *    oldestTimestamp?: number,
-     *    chunkSize?: number,
-     *    startCursor?: string,
-     *    alternativeMethod?: boolean,
+     *     userId: number;
+     *     depth: number;
+     *     oldestTimestamp?: number;
+     *     chunkSize?: number;
+     *     startCursor?: string;
+     *     alternativeMethod?: boolean;
      * }} InstagramUserPostsParams
      */
 
     /**
      * Fetch user posts from the user id.
+     *
      * @param {InstagramUserPostsParams} params
      * @returns {Promise<EDResponse>}
      */
-    userPosts({ userId, depth, oldestTimestamp = undefined, chunkSize = undefined, startCursor = undefined,  alternativeMethod = undefined }) { 
+    userPosts({
+        userId,
+        depth,
+        oldestTimestamp = undefined,
+        chunkSize = undefined,
+        startCursor = undefined,
+        alternativeMethod = undefined,
+    }) {
         const params = filterUndefinedValues({
             depth,
-            ...toSnakeCaseObject({ userId, oldestTimestamp, chunkSize, startCursor, alternativeMethod }),
+            ...toSnakeCaseObject({
+                userId,
+                oldestTimestamp,
+                chunkSize,
+                startCursor,
+                alternativeMethod,
+            }),
         });
         return this.#requester.get("/instagram/user/posts", params);
     }
 
     /**
      * Fetch basic user information from the user id.
+     *
      * @param {{ userId: number }} params
      * @returns {Promise<EDResponse>}
      */
     userBasicStats({ userId }) {
-        return this.#requester.get("/instagram/user/basic-info", toSnakeCaseObject({ userId }));
+        return this.#requester.get(
+            "/instagram/user/basic-info",
+            toSnakeCaseObject({ userId }),
+        );
     }
 
     /**
      * Fetch user information from the username.
+     *
      * @param {{ username: string }} params
      * @returns {Promise<EDResponse>}
      */
@@ -415,49 +456,71 @@ class InstagramClient {
 
     /**
      * Fetch detailed user information from the username.
+     *
      * @param {{ username: string }} params
      * @returns {Promise<EDResponse>}
      */
     userDetailedInfo({ username }) {
-        return this.#requester.get("/instagram/user/detailed-info", { username }); 
+        return this.#requester.get("/instagram/user/detailed-info", {
+            username,
+        });
     }
 
     /**
      * Fetch number of followers from a user id.
+     *
      * @param {{ userId: string }} params
      * @returns {Promise<EDResponse>}
      */
     userFollowers({ userId }) {
-        return this.#requester.get("/instagram/user/followers", toSnakeCaseObject({ userId })); 
+        return this.#requester.get(
+            "/instagram/user/followers",
+            toSnakeCaseObject({ userId }),
+        );
     }
 
     /**
      * @typedef {{
-     *   userId: number,
-     *   depth: string,
-     *   includeFeedVideo?: boolean,
-     *   oldestTimestamp?: number,
-     *   startCursor?: string,
-     *   chunkSize?: number,
+     *     userId: number;
+     *     depth: string;
+     *     includeFeedVideo?: boolean;
+     *     oldestTimestamp?: number;
+     *     startCursor?: string;
+     *     chunkSize?: number;
      * }} InstagramUserReelsParams
      */
 
     /**
      * Fetch user reels from a user id.
+     *
      * @param {InstagramUserReelsParams} params
      * @returns {Promise<EDResponse>}
      */
-    userReels({ userId, depth, includeFeedVideo = undefined, oldestTimestamp = undefined, startCursor = undefined, chunkSize = undefined }) {
+    userReels({
+        userId,
+        depth,
+        includeFeedVideo = undefined,
+        oldestTimestamp = undefined,
+        startCursor = undefined,
+        chunkSize = undefined,
+    }) {
         const params = filterUndefinedValues({
             depth,
-            ...toSnakeCaseObject({ userId, includeFeedVideo, oldestTimestamp, startCursor, chunkSize }),
+            ...toSnakeCaseObject({
+                userId,
+                includeFeedVideo,
+                oldestTimestamp,
+                startCursor,
+                chunkSize,
+            }),
         });
         return this.#requester.get("/instagram/user/reels", params);
     }
 
     /**
      * Fetch posts where the given user ID has been tagged.
-     * @param {{ userId: number, cursor?: string, chunkSize?: number }} params
+     *
+     * @param {{ userId: number; cursor?: string; chunkSize?: number }} params
      * @returns {Promise<EDResponse>}
      */
     userTaggedPosts({ userId, cursor = undefined, chunkSize = undefined }) {
@@ -467,64 +530,154 @@ class InstagramClient {
         });
         return this.#requester.get("/instagram/user/tagged-posts", params);
     }
-    
+
     /**
      * Fetch post information and optionally comments from shortcode.
-     * @param {{ code: string, numComments?: boolean }} params
+     *
+     * @param {{ code: string; numComments?: boolean }} params
      * @returns {Promise<EDResponse>}
      */
     postInfoAndComments({ code, numComments = undefined }) {
-        const params = filterUndefinedValues({ code, n_comments_to_fetch: numComments });
+        const params = filterUndefinedValues({
+            code,
+            n_comments_to_fetch: numComments,
+        });
         return this.#requester.get("/instagram/post/details", params);
     }
 
     /**
      * @typedef {{
-     *   hashtag: string,
-     *   cursor?: string,
-     *   chunkSize?: number,
-     *   getAuthorInfo?: boolean,
-     *   alternativeMethod?: boolean,
+     *     hashtag: string;
+     *     cursor?: string;
+     *     chunkSize?: number;
+     *     getAuthorInfo?: boolean;
+     *     alternativeMethod?: boolean;
      * }} InstagramHashtagPostsParams
      */
 
     /**
      * Fetch most recent posts containing a hashtag.
+     *
      * @param {InstagramHashtagPostsParams} params
      * @returns {Promise<EDResponse>}
      */
-    hashtagPosts({ hashtag, cursor = undefined, chunkSize = undefined, getAuthorInfo = undefined, alternativeMethod = undefined }) {
+    hashtagPosts({
+        hashtag,
+        cursor = undefined,
+        chunkSize = undefined,
+        getAuthorInfo = undefined,
+        alternativeMethod = undefined,
+    }) {
         const params = filterUndefinedValues({
             name: hashtag,
             cursor,
-            ...toSnakeCaseObject({ chunkSize, getAuthorInfo, alternativeMethod }),
+            ...toSnakeCaseObject({
+                chunkSize,
+                getAuthorInfo,
+                alternativeMethod,
+            }),
         });
         return this.#requester.get("/instagram/hashtag/posts", params);
     }
 
     /**
      * Fetch posts from a music id.
-     * @param {{ musicId: string, cursor?: string }} params
+     *
+     * @param {{ musicId: string; cursor?: string }} params
      * @returns {Promise<EDResponse>}
      */
     musicPosts({ musicId, cursor = undefined }) {
         const params = filterUndefinedValues({ id: musicId, cursor });
         return this.#requester.get("/instagram/music/posts", params);
     }
-    
+
     /**
      * Fetch users, places and hashtag information from text.
+     *
      * @param {{ text: string }} params
      * @returns {Promise<EDResponse>}
      */
     search({ text }) {
         return this.#requester.get("/instagram/search", { text });
     }
+}
 
+class YoutubeClient {
+    /**
+     * @type {Requester}
+     * @readonly
+     */
+    #requester;
+
+    /** @param {{ requester: Requester }} options */
+    constructor({ requester }) {
+        this.#requester = requester;
+    }
+
+    /**
+     * @typedef {{
+     *     keyword: string;
+     *     depth: number;
+     *     startCursor?: string;
+     *     period?: "overall" | "hour" | "today" | "week" | "month" | "year";
+     *     sorting?: "relevance" | "time" | "views" | "rating";
+     *     getAdditionalInfo?: boolean;
+     * }} YoutubeKeywordSearchParams
+     */
+
+    /**
+     * Fetch videos from a given keyword.
+     *
+     * @param {YoutubeKeywordSearchParams} params
+     * @returns {Promise<EDResponse>}
+     */
+    keywordSeach({
+        keyword,
+        depth,
+        startCursor = undefined,
+        period = undefined,
+        sorting = undefined,
+        getAdditionalInfo = undefined,
+    }) {
+        const params = filterUndefinedValues({
+            keyword,
+            depth,
+            period,
+            sorting,
+            ...toSnakeCaseObject({ startCursor, getAdditionalInfo }),
+        });
+        return this.#requester.get("/youtube/search", params);
+    }
+
+    /**
+     * Fetch the related categories showing on Youtube search keyword.
+     *
+     * @param {{ keyword: string }} params
+     * @returns {Promise<EDResponse>}
+     */
+    featuredCategoriesSearch({ keyword }) {
+        return this.#requester.get("/youtube/search/featured-categories", {
+            name: keyword,
+        });
+    }
+
+    /**
+     * Fetch videos from a given hashtag.
+     *
+     * @param {{ hashtag: string; depth: number; onlyShorts?: boolean }} params
+     * @returns {Promise<EDResponse>}
+     */
+    hashtagSearch({ hashtag, depth, onlyShorts = undefined }) {
+        const params = filterUndefinedValues({
+            name: hashtag,
+            depth,
+            ...toSnakeCaseObject({ onlyShorts }),
+        });
+        return this.#requester.get("/youtube/hashtag/search", params);
+    }
 }
 
 class TwitchClient {
-
     /**
      * @type {Requester}
      * @readonly
@@ -538,7 +691,12 @@ class TwitchClient {
 
     /**
      * Fetch results (videos/channels/games) from a given keyword.
-     * @param {{ keyword: string; depth: number, type: "videos" | "channels" | "games" }} params
+     *
+     * @param {{
+     *     keyword: string;
+     *     depth: number;
+     *     type: "videos" | "channels" | "games";
+     * }} params
      * @returns {Promise<EDResponse>}
      */
     searchKeyword({ keyword, depth, type }) {
@@ -547,17 +705,16 @@ class TwitchClient {
 
     /**
      * Fetch the number of followers for the given channel.
+     *
      * @param {{ username: string }} params
      * @returns {Promise<EDResponse>}
      */
     userFollowers({ username }) {
         return this.#requester.get("/twitch/user/followers", { username });
     }
-
 }
 
 class RedditClient {
-
     /**
      * @type {Requester}
      * @readonly
@@ -569,36 +726,42 @@ class RedditClient {
         this.#requester = requester;
     }
 
-
     /**
      * @typedef {{
-     *  name: string,
-     *  sort: "hot" | "new" | "top" | "rising",
-     *  period: "hour" | "day" | "week" | "month" | "year" | "all",
-     *  cursor: string,
+     *     name: string;
+     *     sort: "hot" | "new" | "top" | "rising";
+     *     period: "hour" | "day" | "week" | "month" | "year" | "all";
+     *     cursor: string;
      * }} RedditSubredditPostsParams
      */
 
     /**
      * Fetch posts from a subreddit.
+     *
      * @param {RedditSubredditPostsParams} params
      * @returns {Promise<EDResponse>}
      */
     subredditPosts({ name, sort, period, cursor }) {
-        return this.#requester.get("/reddit/subreddit/posts", { name, sort, period, cursor });
+        return this.#requester.get("/reddit/subreddit/posts", {
+            name,
+            sort,
+            period,
+            cursor,
+        });
     }
 
     /**
      * Fetch comments from a post.
-     * @param {{ postId: string, cursor: string }} params
-     * @returns {Promise<EDResponse>}
+     *
+     * @param {{ postId: string; cursor: string }} params
      */
-    postComments( { postId, cursor }) {
-        return this.#requester.get("/reddit/post/comments", { id: postId, cursor });
+    postComments({ postId, cursor }) {
+        return this.#requester.get("/reddit/post/comments", {
+            id: postId,
+            cursor,
+        });
     }
-
 }
-
 
 class CustomerClient {
     /**
@@ -641,11 +804,9 @@ export class EDClient {
 
         /** @readonly */
         this.instagram = new InstagramClient({ requester });
-        
+
         /** @readonly */
         this.twitch = new TwitchClient({ requester });
-        
-
     }
 
     /**
