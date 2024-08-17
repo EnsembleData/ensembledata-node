d90ea9cde510abdfc1c1d09722b5edacd944e568
FaydSpeare <fspea123@gmail.com>
2024-08-17 17:36:24 +0200

    node globals for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
+import globals from "globals";
 
 export default [
     js.configs.recommended,
@@ -8,6 +9,11 @@ export default [
         rules: {
             "no-unused-vars": "warn",
             "no-undef": "warn",
+        },
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
         },
     },
 ];
